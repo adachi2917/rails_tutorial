159cd5221a88f81125f78e0af340900e09383eb8
github adachi2917 <asarailove@gmail.com>
2020-12-05 15:44:44 +0900

    Rubocop走らせた(はず)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
 
-  validates :name, { presence: true }
-
-
-
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
 
-
+  validates :name, { presence: true }
 
   validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true }
 
