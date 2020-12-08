6b5804ba12f3f230b1a2fa7cd1c6b4c2003c9fdb
github adachi2917 <asarailove@gmail.com>
2020-12-08 15:59:18 +0900

    表記の統一

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
       flash[:notice] = "投稿を編集しました"
       redirect_to('/posts/index')
     else
-      render('posts/edit')
+      render('/posts/edit')
     end
   end
 
