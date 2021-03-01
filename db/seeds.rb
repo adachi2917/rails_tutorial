13d08b178e776ba70a738cd2c9be981a67e6bf5f
github adachi2917 <asarailove@gmail.com>
2021-03-01 15:53:35 +0900

    レビューをもとに修正
    seed値追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,37 @@
 Tag.create([
 
+
+
              { name: 'タグ1' },
+
+
 
              { name: 'タグ2' },
 
+
+
              { name: 'タグ3' },
+
+
 
              { name: 'タグ4' },
 
+
+
              { name: 'タグ5' }
 
+
+
            ])
+
+Post.create([
+
+              { content: 'もくもく木曜日' }
+
+            ])
+
+Comment.create([
+
+                 { content: 'それisそう' }
+
+               ])
